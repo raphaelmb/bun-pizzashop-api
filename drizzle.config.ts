94d9d4c5e0e5f1da1fab535e47886349b9109bfe
Raphael Berger <raphaelmb@gmail.com>
2024-03-05 11:07:58 -0300

    add migrations + refactor db schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,5 +3,8 @@ import type { Config } from "drizzle-kit"
 export default {
     schema: "./src/db/schema/index.ts",
     out: "./drizzle",
-    driver: "pg"
+    driver: "pg",
+    dbCredentials: {
+        connectionString: "postgresql://docker:docker@localhost:5432/pizzashop"
+    }
 } satisfies Config
