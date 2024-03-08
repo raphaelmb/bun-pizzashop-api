d28888ba9abe78b165bdbd343210971aca452abc
Raphael Berger <raphaelmb@gmail.com>
2024-03-07 21:33:53 -0300

    Add register restaurant route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,34 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { db } from "../db/connection";
+import { restaurants, users } from "../db/schema";
 
 const app = new Elysia()
-    .get("/", () => 'Hello World')
+
+app.post("/restaurants", async ({ body, set }) => {
+    const { restaurantName, managerName, email, phone } = body
+
+    const [ manager ] = await db.insert(users).values({
+        name: managerName,
+        email,
+        phone,
+        role: "manager"
+    }).returning({
+        id: users.id
+    })
+
+    await db.insert(restaurants).values({
+        name: restaurantName,
+        managerId: manager.id
+    })
+
+    set.status = 204
+    }, {
+    body: t.Object({
+        restaurantName: t.String(),
+        managerName: t.String(),
+        email: t.String(),
+        phone: t.String({ format: "email" })
+    })
+})
 
 app.listen(3333, () => console.log("HTTP Server listening on port 3333..."))
