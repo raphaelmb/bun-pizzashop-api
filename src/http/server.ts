44d5e0ed3de2e0cdbcd1777ccc94b0c7b33f7afc
Raphael Berger <raphaelmb@gmail.com>
2024-03-13 12:16:46 -0300

    Add auth from link

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import { Elysia, t } from "elysia";
 import { registerRestaurant } from "./routes/register-restaurant";
 import { sendAuthLink } from "./routes/send-auth-link";
+import { authenticateFromLink } from "./routes/authenticate-from-link";
 
 const app = new Elysia()
     .use(registerRestaurant)
     .use(sendAuthLink)
+    .use(authenticateFromLink)
 
 app.listen(3333, () => console.log("HTTP Server listening on port 3333..."))
